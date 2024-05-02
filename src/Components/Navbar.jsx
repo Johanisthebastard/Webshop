2e1c369d5c6feac46b14442a70fdd855bf509744
Johan Mattsson <mattsson.johan.93@gmail.com>
2024-05-02 10:49:20 +0200

    pussh

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
 	<div>
 	  {/* <NavLink to="/">Home</NavLink> */}
-	  <NavLink className="cart-head" to="/Cart"><img src="./src/assets/cart.png" alt="Cart" width="30px" /></NavLink>
+	  <NavLink className="cart-head" to="/Cart"></NavLink>
 		<br />
 
 	  <NavLink className="admin-head" to="/Admin">Admin</NavLink>
